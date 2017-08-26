b5fd0c32913a0fad2dfd231864dc454edfe94280
Madhav <madhav13vij@gmail.com>
2017-08-26 15:59:00 -0500

    Added files to project

---
+++ jmeter.py
@@ -0,0 +1,122 @@
+import os
+
+import hashlib
+import time
+import memcache
+import pymysql
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+memC = memcache.Client(['memcache6331.zz49ah.cfg.use2.cache.amazonaws.com:11211'], debug=0)
+
+
+def connectDB():
+    return pymysql.connect(host='madhavrds.cwnoqs3zlcce.us-east-2.rds.amazonaws.com', port=3306, user='madhavvij',
+                           password='system123#', db='cloud6331', local_infile=True)
+
+def createDB():
+    conn = connectDB()
+    cur = conn.cursor()
+    cur.execute("""DROP TABLE IF EXISTS data""")
+    conn.commit()
+    query = """ CREATE TABLE Classes (
+    `Branch` VARCHAR(4) CHARACTER SET utf8,
+    `Course` INT,
+    `Section` INT,
+    `Course_Title` VARCHAR(31) CHARACTER SET utf8,
+    `Instructor` VARCHAR(12) CHARACTER SET utf8,
+    `Day_s` VARCHAR(4) CHARACTER SET utf8,
+    `Start_time` VARCHAR(8) CHARACTER SET utf8,
+    `End_Time` VARCHAR(8) CHARACTER SET utf8,
+    `Max` INT,
+    `Enrolled` INT
+      ); """
+
+    cur.execute(query)
+    conn.commit()
+    query = """ LOAD DATA LOCAL INFILE '/home/ubuntu/quiz7/input/Classes.csv' INTO TABLE
+                  Classes FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' ESCAPED
+                  BY '"' Lines terminated by '\n' IGNORE 1 LINES; """
+    cur.execute(query)
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+def fromDB(sql):
+    conn = connectDB()
+    cur = conn.cursor()
+    cur.execute(sql)
+    data = cur.fetchall()
+    conn.commit()
+    cur.close()
+    conn.close()
+    return data
+
+
+def fromMemcache(sql):
+    conn = connectDB()
+    cur = conn.cursor()
+
+    hash = hashlib.sha256(sql).hexdigest()
+    # print(hash)
+    key = 'cache:' + hash
+    # print("Key= ")
+    # print(key)
+
+    if memC.get(key):
+        # print("used memcache")
+        return memC.get(key)
+
+    else:
+        # print("add to memcache")
+        cur.execute(sql)
+        data = cur.fetchall()
+        conn.commit()
+        cur.close()
+        conn.close()
+        memC.set(key, data, time=500)
+
+        return memC.get(key)
+
+
+@app.route('/getAll', methods=['POST','GET'])
+def exampleDB():
+    start_time = time.time()
+    result = 0
+    output = []
+    if request.method == 'POST':
+        #instructor = request.args.get('name', '')
+        instructor = 'xxxxxx'
+        course = '0000'
+
+        if request.form['name']:
+            instructor = request.form['name']
+        if request.form['course']:
+            course = request.form['course']
+        query = 'select * from Classes WHERE Instructor like "%'+instructor+'%" or Course = "'+course+'"'
+        result = fromMemcache(query)
+        for row in result:
+            tuple = (row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9])
+            output.append(tuple)
+    end_time = time.time()
+    total_time = end_time-start_time
+    print(total_time)
+    output.append(total_time)
+    return render_template('display.html', output=output)
+
+
+
+
+@app.route('/')
+def main():
+    #createDB()
+    return render_template('index.html')
+
+
+port = os.getenv('PORT', '80')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(port))
+
+#####################################################################
+
